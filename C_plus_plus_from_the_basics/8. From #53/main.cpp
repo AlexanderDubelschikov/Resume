f683aab4ff7d6fdada52534bdab85e066bb20a16
AlexanderDubelschikov <109314029+AlexanderDubelschikov@users.noreply.github.com>
2022-08-04 20:38:10 +0300

    Пересборка

---
+++ C_plus_plus_from_the_basics/8. From #53/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  8. From #53
+//
+//  Created by Sokrat on 04.08.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
