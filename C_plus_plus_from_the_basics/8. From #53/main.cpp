4c30293bdbf413231e545b147091b9b34c68b91b
AlexanderDubelschikov <109314029+AlexanderDubelschikov@users.noreply.github.com>
2022-08-08 17:36:09 +0300

    59 END!

--- C_plus_plus_from_the_basics/8. From #53/main.cpp
+++ C_plus_plus_from_the_basics/8. From #53/main.cpp
@@ -1,4 +1,4 @@
-//Динамическая память. Операторы выделения new и delete. Null pointer.Динамические массивы. Двумерный динамический массив. Операции с динамическими массивами. Копирование массивов с одинаковым и разным количеством элементов. Свап массивов с одинаковым количеством элементов.
+//Динамическая память. Операторы выделения new и delete. Null pointer.Динамические массивы. Двумерный динамический массив. Операции с динамическими массивами. Копирование массивов с одинаковым и разным количеством элементов. Свап массивов с одинаковым количеством элементов. Добавление и удаление элементов массива.
 
 #include <iostream>
 #include <ctime>
@@ -6,7 +6,7 @@
 using namespace std;
 const char NEW_LINE = '\n';
 const char TAB = '\t';
-int arr_sup, value;
+int arr_sup, value, position;
 
 void NumArray(int *size);
 void FillArray(int* const arr, const int size);
@@ -14,6 +14,13 @@ void ShowArray(const int* const arr, const int size);
 void CopyArray(int* const arr, int* const arr2, const int size);
 void SwapArray(int* const arr, int* const arr2, const int size);
 void CopyDifArray(int* arr, int* arr2, const int size);
+void NumArray2(int &size);
+void Push_Back(int *&arr, int &size, const int value);
+void Delete_Back(int *&arr, int &size);
+void Push_Start(int *&arr, int &size, const int value);
+void Delete_Start(int *&arr, int &size);
+void Push_In(int *&arr, int &size, int const value, int const position);
+void Push_Out(int *&arr, int &size,int const position);
 
 int main()
 {
@@ -216,9 +223,36 @@ int main()
      
      delete [] arr;*/
     
-    //Добавление элементов через функции
-     
-
+    //Добавление элементов и удаление через функции, более правильное
+    /*int size=0;
+    value = 10;
+    NumArray2(size);
+    int *arr = new int [size];
+    FillArray(arr, size);
+    cout << "Изначальный массив: " << NEW_LINE;
+    ShowArray(arr, size);
+    Push_Back(arr, size, value);
+    cout << "Добавлен элемент в конце: " << NEW_LINE;
+    ShowArray(arr, size);
+    Delete_Back(arr, size);
+    cout << "Удален элемент в конце: " << NEW_LINE;
+    ShowArray(arr, size);
+    Push_Start(arr, size, value);
+    cout << "Добавление элемента в начало: " << NEW_LINE;
+    ShowArray(arr, size);
+    cout << "Удаление элемента в начале: " << NEW_LINE;
+    Delete_Start(arr, size);
+    ShowArray(arr, size);
+    cout << "На какой позиции по индексу встаить эелемент? " << NEW_LINE;
+    cin >> position;
+    Push_In(arr,size,value,position);
+    ShowArray(arr, size);
+    cout << "На какой позиции по индексу удалить эелемент? " << NEW_LINE;
+    cin >> position;
+    Push_Out(arr,size,position);
+    ShowArray(arr, size);
+    delete[] arr;*/
+    
     return 0;
 
 }
@@ -228,6 +262,13 @@ void NumArray(int* const size)
 {
     cout << "Введи количество переменных: " << TAB;
     cin >> *size;
+}
+
+//Второй вариант, по ссылку на переменную без указателя
+void NumArray2(int &size)
+{
+    cout << "Введи количество переменных: " << TAB;
+    cin >> size;
 }
 
 //Заполнение массива
@@ -278,3 +319,94 @@ void CopyDifArray(int* arr, int* arr2, const int size)
     CopyArray(arr, arr2, size);
 }
 
+//Добавление элемента в конце
+void Push_Back(int *&arr, int &size, const int value)
+{
+    int *NewArr = new int [size+1];
+    CopyArray(NewArr, arr, size);
+    NewArr[size]=value;
+    delete [] arr;
+    arr = NewArr;
+    size++;
+    
+}
+
+//Удаление элемента с конца
+void Delete_Back(int *&arr, int &size)
+{
+    int *NewArr = new int [size-1];
+    size--;
+    CopyArray(NewArr, arr, size);
+    delete [] arr;
+    arr = NewArr;
+    
+}
+
+//Добавление элемента в начало
+void Push_Start(int *&arr, int &size, const int value)
+{
+    int *NewArr = new int [size+1];
+    for (int i=0; i<size; i++)
+    {
+        NewArr[i+1]=arr[i];
+    }
+    delete[] arr;
+    NewArr[0]=value;
+    arr=NewArr;
+    size++;
+}
+
+//Удаление элемента с конца
+void Delete_Start(int *&arr, int &size)
+{
+    int *NewArr = new int [size-1];
+    for (int i=0; i<size-1; i++)
+    {
+        NewArr[i]=arr[i+1];
+    }
+    
+    delete [] arr;
+    arr=NewArr;
+    size--;
+    
+}
+
+//Добавить в неопределенное место
+void Push_In(int *&arr, int &size, int const value, int const position)
+{
+    int *NewArr=new int [size+1];
+    for (int i=0; i<position; i++)
+    {
+        NewArr[i]=arr[i];
+    }
+    
+    NewArr[position] = value;
+    
+    for (int i=position+1; i<size+1; i++)
+    {
+        NewArr[i]=arr[i-1];
+    }
+    
+    delete [] arr;
+    arr = NewArr;
+    size++;
+}
+
+//Удаление элемента в определенном месте
+void Push_Out(int *&arr, int &size,int const position)
+{
+    int *NewArr= new int [size-1];
+    for (int i=0; i<position; i++)
+    {
+        NewArr[i]=arr[i];
+    }
+    
+    for (int i=position+1; i<size-1; i++)
+    {
+        NewArr[i-1]=arr[i];
+        
+    }
+    delete[] arr;
+    arr=NewArr;
+    size--;
+}
