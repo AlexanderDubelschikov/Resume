ad02fe4d61419f17218f3cab78ca92365083f3a1
AlexanderDubelschikov <109314029+AlexanderDubelschikov@users.noreply.github.com>
2022-08-09 01:54:41 +0300

    Lesson 63

--- C_plus_plus_from_the_basics/9. From #60/main.cpp
+++ C_plus_plus_from_the_basics/9. From #60/main.cpp
@@ -1,14 +1,66 @@
-//
-//  main.cpp
-//  9. From #60
-//
-//  Created by Sokrat on 08.08.2022.
-//
-
+//Нуль треминал. Char массив. Строки. ASCII.
+#include <clocale>
+#include <string>
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+const char NEW_LINE = '\n';
+const char TAB = '\t';
+
+void Foo(char *str);
+void Strlen_my(char *str);
+
+int main()
+{
+    setlocale(LC_ALL,"ru") ;
+    
+    //Знакомство с символами и строками
+    /*char symbol = 'a';
+    cout << symbol << NEW_LINE;
+    char string [] = "string";
+    cout << string << NEW_LINE;
+    char string2 [] = {'H','e','l','l','o','\0'};
+    cout << string2 << NEW_LINE;
+    cout << strlen(string2) << NEW_LINE;*/
+    
+    //Преведение типов (C)
+    /*double a = 33.3;
+    cout << a << NEW_LINE;
+    cout << (int)a << NEW_LINE;
+    cout << (bool)a << NEW_LINE;
+    cout << (char)a << NEW_LINE;
+    int b = 34.333;
+    cout << b << NEW_LINE;*/
+    
+    //ASCII (из-за работы в xCode проблемы с ру сиволами, несмотря на setlocale)
+    /*cout << sizeof(char) << NEW_LINE;
+    for (int i=0; i <= 255; i++)
+    {
+        cout << "Code = " << i << " Char = " << (unsigned char)i << NEW_LINE;
+    }*/
+    
+    //Строки и указатели. Реализация strlen своими руками
+    
+    
 }
+
+//Вывод количества элементов строки
+void Foo(char *str)
+{
+    cout << strlen(str) << NEW_LINE;
+}
+
+//Реализация strlen своими руками
+void Strlen_my(char *str)
+{
+    int i=0;
+    int k=0;
+    while (str[i]!='\0')
+    {
+        k++;
+        i++;
+    }
+    
+    cout << k << NEW_LINE;
+}
+
