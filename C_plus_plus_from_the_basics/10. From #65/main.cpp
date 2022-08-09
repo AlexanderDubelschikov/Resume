911805426366129d4b2081d89a7c2d8075fd18c0
AlexanderDubelschikov <109314029+AlexanderDubelschikov@users.noreply.github.com>
2022-08-09 15:23:02 +0300

    repair

--- C_plus_plus_from_the_basics/10. From #65/main.cpp
+++ C_plus_plus_from_the_basics/10. From #65/main.cpp
@@ -1,7 +1,10 @@
+//Указатели на функцию
+
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
+    
+    
     return 0;
 }
