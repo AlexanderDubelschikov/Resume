681ed660f139062e1fcab6ae0af2eb30438e9b5d
AlexanderDubelschikov <109314029+AlexanderDubelschikov@users.noreply.github.com>
2022-08-10 17:14:34 +0300

    OOP start

---
+++ C_plus_plus_OOP/1. From #72/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  1. From #72
+//
+//  Created by Sokrat on 10.08.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
