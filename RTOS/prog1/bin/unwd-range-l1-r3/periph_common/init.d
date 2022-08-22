f4bf6738c1a0932e5c26bde833312820f2430d6c
AlexanderDubelschikov <109314029+AlexanderDubelschikov@users.noreply.github.com>
2022-08-22 10:16:42 +0300

    Clean Up

--- RTOS/prog1/bin/unwd-range-l1-r3/periph_common/init.d
+++
@@ -1,4 +0,0 @@
-/Users/sokrat/Desktop/RIOT/miem/prog1/bin/unwd-range-l1-r3/periph_common/init.o: \
- /Users/sokrat/Desktop/RIOT/drivers/periph_common/init.c \
- /Users/sokrat/Desktop/RIOT/miem/prog1/bin/unwd-range-l1-r3/riotbuild/riotbuild.h
-/Users/sokrat/Desktop/RIOT/miem/prog1/bin/unwd-range-l1-r3/riotbuild/riotbuild.h:
--- RTOS/prog6/bin/unwd-range-l1-r3/stm32_common/stmclk.d
+++
@@ -1,4 +0,0 @@
-/Users/sokrat/Desktop/RIOT/miem/sensors/bin/unwd-range-l1-r3/stm32_common/stmclk.o: \
- /Users/sokrat/Desktop/RIOT/cpu/stm32_common/stmclk.c \
- /Users/sokrat/Desktop/RIOT/miem/sensors/bin/unwd-range-l1-r3/riotbuild/riotbuild.h
-/Users/sokrat/Desktop/RIOT/miem/sensors/bin/unwd-range-l1-r3/riotbuild/riotbuild.h:
